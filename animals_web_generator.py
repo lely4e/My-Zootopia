56362eb84be2d326c4d8aa4779263783845b59ee
Olga Masterschool <lely4e@gmail.com>
2025-08-20 15:29:09 +0200

    Bring animal data into html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,23 +1,39 @@
 import json
 
+
 def load_data(file_path):
     """ Loads a JSON file """
     with open(file_path, "r") as handle:
         return json.load(handle)
-    
 
-animals_data = load_data("animals_data.json")
-#print(animals_data)
+
+def read_html(file_path):
+    """ Read an HTML file """
+    with open(file_path, "r") as handle:
+        return handle.read()
 
 
 def get_animal_info(data):
     """ Displays name, diet, location and type of the animal from the data """
+    output = ""
     for animal in data:
-        print("\nName:", animal["name"]) 
-        print("Diet:", animal["characteristics"]["diet"])
-        print("Location:", animal["locations"][0])
+        output += f"Name: {animal["name"]}\n" 
+        output += f"Diet: {animal["characteristics"]["diet"]}\n"
+        output += f"Location: {animal["locations"][0]}\n"
         if "type" in animal["characteristics"]:
-            print("Type:", animal["characteristics"]["type"])
+            output += f"Type: {animal["characteristics"]["type"]}\n"         
+    return output
             
-            
-get_animal_info(animals_data)
+
+def new_html_data(file_path):
+    """ Creating a new HTML file with the necessery information """
+    with open(file_path, "w") as handle:
+        html_data = read_html("animals_template.html")  
+        animals_info = get_animal_info(animals_data)
+        new = html_data.replace("__REPLACE_ANIMALS_INFO__", animals_info)
+        return handle.write(new)
+
+    
+animals_data = load_data("animals_data.json")
+print(new_html_data("animals.html"))
+
