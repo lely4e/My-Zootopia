cea0d7d1d020bc715d805a4b743307c261528659
Olga Masterschool <lely4e@gmail.com>
2025-08-20 16:14:28 +0200

    Creating new function to serialize a single animal object

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -13,19 +13,26 @@ def read_html(file_path):
         return handle.read()
 
 
+def serialize_animal(animal_obj):
+    """ Serialization of a single animal onject """
+    output = ''
+    output += '<li class="cards__item">\n'
+    output += f'<div class="card__title">{animal_obj["name"]}</div>\n'
+    output += '<p class="card__text">\n' 
+    output += f'<strong>Diet:</strong> {animal_obj["characteristics"]["diet"]}<br/>\n'
+    output += f'<strong>Location:</strong> {animal_obj["locations"][0]}<br/>\n'
+    if "type" in animal_obj["characteristics"]:
+        output += f'<strong>Type:</strong> {animal_obj["characteristics"]["type"]}<br/>\n' 
+    output += '</p>'    
+    output += '</li>\n'   
+    return output
+    
+    
 def get_animal_info(data):
     """ Displays name, diet, location and type of the animal from the data """
     output = ""
-    for animal in data:
-        output += '<li class="cards__item">\n'
-        output += f'<div class="card__title">{animal["name"]}</div>\n'
-        output += '<p class="card__text">\n' 
-        output += f'<strong>Diet:</strong> {animal["characteristics"]["diet"]}<br/>\n'
-        output += f'<strong>Location:</strong> {animal["locations"][0]}<br/>\n'
-        if "type" in animal["characteristics"]:
-            output += f'<strong>Type:</strong> {animal["characteristics"]["type"]}<br/>\n' 
-        output += '</p>'    
-        output += '</li>\n'   
+    for animal in data:   
+        output += serialize_animal(animal) 
     return output
             
 
@@ -33,11 +40,16 @@ def new_html_data(file_path):
     """ Creating a new HTML file with the necessery information """
     with open(file_path, "w") as handle:
         html_data = read_html("animals_template.html")  
+        animals_data = load_data("animals_data.json")
         animals_info = get_animal_info(animals_data)
         new = html_data.replace("__REPLACE_ANIMALS_INFO__", animals_info)
         return handle.write(new)
 
     
-animals_data = load_data("animals_data.json")
-print(new_html_data("animals.html"))
+def main():
+    print(new_html_data("animals.html"))
 
+
+if __name__ == "__main__":
+    main()
+    
