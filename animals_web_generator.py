7f98f31a9bc3864fe919737a4cff45d17ab3a2bf
Olga Masterschool <lely4e@gmail.com>
2025-08-21 12:36:48 +0200

    Filtering cards to show by skin type

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -18,19 +18,17 @@ def serialize_animal(animal_obj):
     output = ''
     output += '<li class="cards__item">\n'
     output += f'<div class="card__title" style="margin: 10px;">{animal_obj["name"]}</div>\n'
-    output += '<div class="card__text">\n' 
+    output += '<div class="card__text">\n'
     output += '<ul>\n'
-    if "genus" in animal_obj["taxonomy"]:
-        output += f'<li><strong>Genus:</strong> {animal_obj["taxonomy"]["genus"]}</li>\n'
-    output += f'<li><strong>Diet:</strong> {animal_obj["characteristics"]["diet"]}</li>\n'
+    output += f'<li><strong>Genus:</strong> {animal_obj["taxonomy"].get("genus")}</li>\n'
+    output += f'<li><strong>Diet:</strong> {animal_obj["characteristics"].get("diet")}</li>\n'
     output += f'<li><strong>Location:</strong> {animal_obj["locations"][0]}</li>\n'
-    if "type" in animal_obj["characteristics"]:
-        output += f'<li><strong>Type:</strong> {animal_obj["characteristics"]["type"]}</li>\n' 
-    if "color" in animal_obj["characteristics"]:
-        output += f'<li><strong>Color:</strong> {animal_obj["characteristics"]["color"]}</li>\n'
-    output += '</ul>'   
-    output += '</div>' 
-    output += '</li>\n'   
+    output += f'<li><strong>Type:</strong> {animal_obj["characteristics"].get("type")}</li>\n'
+    output += f'<li><strong>Color:</strong> {animal_obj["characteristics"].get("color")}</li>\n'
+    output += f'<li><strong>Skin type:</strong> {animal_obj["characteristics"].get("skin_type")}</li>\n'
+    output += '</ul>'
+    output += '</div>'
+    output += '</li>\n'
     return output
     
     
@@ -42,18 +40,66 @@ def get_animal_info(data):
     return output
             
 
-def new_html_data(file_path):
+def get_skin_types(data):
+    """ Displays unique skin types from the data """
+    skin_types = set()
+    for animal in data:
+        if "skin_type" in animal["characteristics"]:
+            skin_types.add(animal["characteristics"]["skin_type"])
+    return sorted(skin_types)
+
+
+def filtered_animals(data, guess):
+    filtered_animals_data = []
+    for animal in data:
+        if guess ==  animal["characteristics"]["skin_type"].lower():
+            filtered_animals_data.append(animal)
+    return filtered_animals_data
+
+
+def new_html_data(file_path, filtered_animals_data):
     """ Creating a new HTML file with the necessery information """
     with open(file_path, "w") as handle:
-        html_data = read_html("animals_template.html")  
-        animals_data = load_data("animals_data.json")
-        animals_info = get_animal_info(animals_data)
+        html_data = read_html("animals_template.html")
+        animals_info = get_animal_info(filtered_animals_data)
         new = html_data.replace("__REPLACE_ANIMALS_INFO__", animals_info)
         return handle.write(new)
 
     
+def error_page(file_path):
+    """ Renders an error page """
+    with open(file_path, "w") as handle:
+        output = ''
+        output += '<li class="cards__item">\n'
+        output += f'<div class="card__title" style="margin: 10px;">Cards are not found!</div>\n'
+        output += '</div>'
+        output += '</li>\n'
+
+        html_data = read_html("animals_template.html")
+        new = html_data.replace("__REPLACE_ANIMALS_INFO__", output)
+        return handle.write(new)
+    
+    
 def main():
-    print(new_html_data("animals.html"))
+    """ Main logic of the program """
+    animals_data = load_data("animals_data.json")
+
+    skin_types = get_skin_types(animals_data)
+    if skin_types:
+        print("The following skin types are available:")
+        for skin_type in skin_types:
+            print(f"- {skin_type}")
+    else:
+        print("No skin types available")
+
+    user_choice = input("Enter skin type: ").strip().lower()
+
+    suitable_animals = filtered_animals(animals_data, user_choice)
+    if suitable_animals:
+        print(new_html_data("animals.html", suitable_animals))
+    else:
+        print("Invalid skin type")
+        print(error_page("animals.html"))
 
 
 if __name__ == "__main__":
